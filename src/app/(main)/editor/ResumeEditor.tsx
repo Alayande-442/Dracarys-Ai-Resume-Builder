3743582d61a62e9c2981ff1142bb04917ebb18e4
Alayande-442 <alayandesteven@gmail.com>
2025-07-06 20:15:08 +0100

    adding functionality to personal info & general info Form

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import GeneralInfoForm from "./forms/GeneralInfoForm";
+import PersonalInfoForm from "./forms/PersonalInfoForm";
 export default function ResumeEditor() {
   return (
     <div className="flex grow flex-col">
@@ -16,7 +18,9 @@ export default function ResumeEditor() {
 
       <main className="relative grow">
         <div className="absolute bottom-0 top-0 flex w-full">
-          <div className="w-full md:w-1/2">Left</div>
+          <div className="w-full overflow-y-auto p-2 md:w-1/2">
+            <PersonalInfoForm />
+          </div>
           <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">Right</div>
         </div>
